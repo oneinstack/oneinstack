38c2f1bf646a2d8c450367d1d06dad7f2bcfd874
lj2007331 <lj2007331@gmail.com>
2015-07-10 13:42:05 +0800

    Update apps.conf

--- backup.sh
+++ backup.sh
@@ -8,11 +8,12 @@
 # Project home page:
 #       http://oneinstack.com
 
-. ./options.conf
+cd tools
+. ../options.conf
+
 DB_Local_BK() {
 	for D in `echo $db_name | tr ',' ' '`
 	do
-		cd $oneinstack_dir/tools
 		./db_bk.sh $D
 	done
 }
@@ -20,19 +21,17 @@ DB_Local_BK() {
 DB_Remote_BK() {
         for D in `echo $db_name | tr ',' ' '`
         do
-                cd $oneinstack_dir/tools
                 ./db_bk.sh $D
 		DB_GREP="DB_${D}_`date +%Y`"
                 DB_FILE=`ls -lrt $backup_dir | grep ${DB_GREP} | tail -1 | awk '{print $NF}'`
-                echo "file:::$backup_dir/$DB_FILE $backup_dir push" >> $oneinstack_dir/tools/config.txt
-                echo "com:::[ -e "$backup_dir/$DB_FILE" ] && rm -rf $backup_dir/DB_${D}_$(date +%Y%m%d --date="$expired_days days ago")_*.tgz" >> $oneinstack_dir/tools/config.txt
+                echo "file:::$backup_dir/$DB_FILE $backup_dir push" >> config_bakcup.txt
+                echo "com:::[ -e "$backup_dir/$DB_FILE" ] && rm -rf $backup_dir/DB_${D}_$(date +%Y%m%d --date="$expired_days days ago")_*.tgz" >> config_bakcup.txt
         done
 }
 
 WEB_Local_BK() {
 	for W in `echo $website_name | tr ',' ' '`
         do
-                cd $oneinstack_dir/tools
                 ./website_bk.sh $W
         done
 }
@@ -40,8 +39,7 @@ WEB_Local_BK() {
 WEB_Remote_BK() {
         for W in `echo $website_name | tr ',' ' '`
         do
-		cd $oneinstack_dir/tools
-                echo "file:::$wwwroot_dir/$W $backup_dir push" >> $oneinstack_dir/tools/config.txt
+                echo "file:::$wwwroot_dir/$W $backup_dir push" >> config_bakcup.txt
         done
 }
 
@@ -49,15 +47,15 @@ if [ "$local_bankup_yn" == 'y' -a "$remote_bankup_yn" == 'n' ];then
 	WEB_Local_BK
 	DB_Local_BK
 elif [ "$local_bankup_yn" == 'n' -a "$remote_bankup_yn" == 'y' ];then
-	echo "com:::[ ! -e "$backup_dir" ] && mkdir -p $backup_dir" > $oneinstack_dir/tools/config.txt
+	echo "com:::[ ! -e "$backup_dir" ] && mkdir -p $backup_dir" > config_bakcup.txt
 	DB_Remote_BK
 	WEB_Remote_BK
-	./mabs.sh -T -1 | tee mabs.log	
+	./mabs.sh -c config_bakcup.txt -T -1 | tee mabs.log
 elif [ "$local_bankup_yn" == 'y' -a "$remote_bankup_yn" == 'y' ];then
-	echo "com:::[ ! -e "$backup_dir" ] && mkdir -p $backup_dir" > $oneinstack_dir/tools/config.txt
+	echo "com:::[ ! -e "$backup_dir" ] && mkdir -p $backup_dir" > config_bakcup.txt
 	WEB_Local_BK
 	WEB_Remote_BK
 	DB_Local_BK
 	DB_Remote_BK
-	./mabs.sh -T -1 | tee mabs.log	
+	./mabs.sh -c config_bakcup.txt -T -1 | tee mabs.log	
 fi
