f0ebcfe33637b1633c2ddfb8c6d713f3d4439cc4
lj2007331@gmail.com <lj2007331@gmail.com>
2018-03-13 08:36:55 +0800

    Fix ioncube zts

--- include/download.sh
+++ include/download.sh
@@ -11,7 +11,7 @@
 Download_src() {
   [ -s "${src_url##*/}" ] && echo "[${CMSG}${src_url##*/}${CEND}] found" || { wget -4 --tries=6 -c --no-check-certificate $src_url; sleep 1; }
   if [ ! -e "${src_url##*/}" ]; then
-    echo "${CFAILURE}Auto download failed! You can manually download ${src_url##*/} into the oneinstack/src directory.${CEND}"
+    echo "${CFAILURE}Auto download failed! You can manually download ${src_url} into the oneinstack/src directory.${CEND}"
     kill -9 $$
   fi
 }
