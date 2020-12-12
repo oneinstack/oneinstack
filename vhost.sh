9405693eb7a75cf0dc5d44d2e4924d33faeeb120
lj2007331@gmail.com <lj2007331@gmail.com>
2020-12-12 14:25:57 +0800

    [feat] Update

--- vhost.sh
+++ vhost.sh
@@ -465,7 +465,7 @@ What Are You Doing?
         echo "Create Virtul Host directory......"
         mkdir -p ${vhostdir}
         echo "set permissions of Virtual Host directory......"
-        chown -R ${run_user}.${run_group} ${vhostdir}
+        chown -R ${run_user}:${run_group} ${vhostdir}
         break
       fi
     done
