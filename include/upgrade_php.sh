abad645b0945e9edd88a49e59edb52fc9f9d48f5
Kevin <77494658+Kevin-Ben@users.noreply.github.com>
2021-03-06 18:34:47 +0800

    Update upgrade_php.sh
    use sohu

--- include/upgrade_php.sh
+++ include/upgrade_php.sh
@@ -20,7 +20,7 @@ Upgrade_PHP() {
     [ "${php_flag}" != 'y' ] && read -e -p "Please input upgrade PHP Version(Default: $Latest_php_ver): " NEW_php_ver
     NEW_php_ver=${NEW_php_ver:-${Latest_php_ver}}
     if [ "${NEW_php_ver%.*}" == "${OLD_php_ver%.*}" ]; then
-      [ ! -e "php-${NEW_php_ver}.tar.gz" ] && wget --no-check-certificate -c https://secure.php.net/distributions/php-${NEW_php_ver}.tar.gz > /dev/null 2>&1
+      [ ! -e "php-${NEW_php_ver}.tar.gz" ] && wget --no-check-certificate -c https://mirrors.sohu.com/php/php-${NEW_php_ver}.tar.gz > /dev/null 2>&1
       if [ -e "php-${NEW_php_ver}.tar.gz" ]; then
         echo "Download [${CMSG}php-${NEW_php_ver}.tar.gz${CEND}] successfully! "
       else
