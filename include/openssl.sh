bdc54083388833ac4301a737ea6ec69743447230
lj2007331@gmail.com <lj2007331@gmail.com>
2021-10-02 16:35:24 +0800

    [fix] Update

--- include/openssl.sh
+++ include/openssl.sh
@@ -8,7 +8,7 @@
 #       https://oneinstack.com
 #       https://github.com/oneinstack/oneinstack
 
-Install_openSSL102() {
+Install_openSSL() {
   if [ -e "${openssl_install_dir}/lib/libssl.a" ]; then
     echo "${CWARNING}openSSL already installed! ${CEND}"
   else
@@ -21,36 +21,12 @@ Install_openSSL102() {
     make -j ${THREAD} && make install
     popd > /dev/null
     if [ -f "${openssl_install_dir}/lib/libcrypto.a" ]; then
-      echo "${CSUCCESS}openssl installed successfully! ${CEND}"
+      echo "${CSUCCESS}openSSL installed successfully! ${CEND}"
       /bin/cp cacert.pem ${openssl_install_dir}/cert.pem
       rm -rf openssl-${openssl_ver}
     else
       echo "${CFAILURE}openSSL install failed, Please contact the author! ${CEND}" && lsb_release -a
-      kill -9 $$
-    fi
-    popd > /dev/null
-  fi
-}
-
-Install_openSSL() {
-  if [ -e "${openssl_install_dir}/lib/libssl.a" ]; then
-    echo "${CWARNING}openSSL already installed! ${CEND}"
-  else
-    pushd ${oneinstack_dir}/src > /dev/null
-    tar xzf openssl-${openssl11_ver}.tar.gz
-    pushd openssl-${openssl11_ver} > /dev/null
-    make clean
-    ./config -Wl,-rpath=${openssl_install_dir}/lib -fPIC --prefix=${openssl_install_dir} --openssldir=${openssl_install_dir}
-    make depend
-    make -j ${THREAD} && make install
-    popd > /dev/null
-    if [ -f "${openssl_install_dir}/lib/libcrypto.a" ]; then
-      echo "${CSUCCESS}openssl installed successfully! ${CEND}"
-      /bin/cp cacert.pem ${openssl_install_dir}/cert.pem
-      rm -rf openssl-${openssl11_ver}
-    else
-      echo "${CFAILURE}openSSL install failed, Please contact the author! ${CEND}" && lsb_release -a
-      kill -9 $$
+      kill -9 $$; exit 1;
     fi
     popd > /dev/null
   fi
