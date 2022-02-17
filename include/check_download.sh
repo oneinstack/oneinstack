7037a212b62424d2a99f670984be63f5a6c4bf65
lj2007331@gmail.com <lj2007331@gmail.com>
2022-02-17 10:04:56 +0800

    [fixed] Update iconv

--- include/check_download.sh
+++ include/check_download.sh
@@ -714,10 +714,10 @@ checkDownload() {
   fi
 
   # nodejs
-  if [ "${node_flag}" == 'y' ]; then
+  if [ "${nodejs_flag}" == 'y' ]; then
     echo "Download Nodejs..."
     [ "${IPADDR_COUNTRY}"x == "CN"x ] && DOWN_ADDR_NODE=https://nodejs.org/dist || DOWN_ADDR_NODE=https://mirrors.tuna.tsinghua.edu.cn/nodejs-release
-    src_url=${DOWN_ADDR_NODE}/v${node_ver}/node-v${node_ver}-linux-${SYS_BIT_n}.tar.gz && Download_src
+    src_url=${DOWN_ADDR_NODE}/v${nodejs_ver}/node-v${nodejs_ver}-linux-${SYS_BIT_n}.tar.gz && Download_src
   fi
 
   # pureftpd
