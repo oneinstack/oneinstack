68f7b0211c1550f09048c9619e142c20dd8c1624
weixiao5 <512720913@qq.com>
2022-04-15 12:35:24 +0800

    add nginx tengine stream module support
    add nginx tengine stream module support
    
    demo:
    stream{
     server {
            listen      12345;
            proxy_pass  xx.xx.xx.xx:prot;
            ssl_preread on;
        }
    }

--- include/tengine.sh
+++ include/tengine.sh
@@ -23,7 +23,7 @@ Install_Tengine() {
   #sed -i 's@TENGINE "/" TENGINE_VERSION@"Tengine/unknown"@' src/core/nginx.h
 
   [ ! -d "${tengine_install_dir}" ] && mkdir -p ${tengine_install_dir}
-  ./configure --prefix=${tengine_install_dir} --user=${run_user} --group=${run_group} --with-http_v2_module --with-http_ssl_module --with-http_gzip_static_module --with-http_realip_module --with-http_flv_module --with-http_mp4_module --with-openssl=../openssl-${openssl11_ver} --with-pcre=../pcre-${pcre_ver} --with-pcre-jit --with-jemalloc ${nginx_modules_options}
+  ./configure --prefix=${tengine_install_dir} --user=${run_user} --group=${run_group} --with-http_v2_module --with-http_ssl_module --with-stream --with-stream_ssl_preread_module --with-stream_ssl_module --with-http_gzip_static_module --with-http_realip_module --with-http_flv_module --with-http_mp4_module --with-openssl=../openssl-${openssl11_ver} --with-pcre=../pcre-${pcre_ver} --with-pcre-jit --with-jemalloc ${nginx_modules_options}
   make && make install
   if [ -e "${tengine_install_dir}/conf/nginx.conf" ]; then
     popd > /dev/null
