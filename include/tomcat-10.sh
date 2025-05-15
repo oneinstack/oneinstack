6df8cd230a46c35648dbf5bf6178ee7144ff8a68
冯培榕 <15089563+feng-peirong@user.noreply.gitee.com>
2025-05-15 15:03:22 +0800

    #bug3修改的文件为==> tomcat-10.sh #由：OpenSSL路径配置错误自动安装 Apache Tomcat 10.1.16 并配置系统服务 #设置内存参数（Xms 和 Xmx）及环境变量逻辑

--- include/tomcat-10.sh
+++ include/tomcat-10.sh
@@ -1,129 +1,146 @@
 #!/bin/bash
-# Author:  yeho <lj2007331 AT gmail.com>
-# BLOG:  https://linuxeye.com
-#
-# Notes: OneinStack for CentOS/RedHat 7+ Debian 9+ and Ubuntu 16+
-#
-# Project home page:
-#       https://oneinstack.com
-#       https://github.com/oneinstack/oneinstack
 
-Install_Tomcat10() {
-  pushd ${oneinstack_dir}/src > /dev/null
-  . /etc/profile
-  id -g ${run_group} >/dev/null 2>&1
-  [ $? -ne 0 ] && groupadd ${run_group}
-  id -u ${run_user} >/dev/null 2>&1
-  [ $? -ne 0 ] && useradd -g ${run_group} -M -s /bin/bash ${run_user} || { [ -z "$(grep ^${run_user} /etc/passwd | grep '/bin/bash')" ] && usermod -g ${run_group} -s /bin/bash ${run_user}; }
+# Color
+CFAILURE='\033[31m'
+CSUCCESS='\033[32m'
+CWARNING='\033[33m'
+CINFO='\033[36m'
+CEND='\033[0m'
 
-  # install apr
-  if [ ! -e "${apr_install_dir}/bin/apr-1-config" ]; then
-    tar xzf apr-${apr_ver}.tar.gz
-    pushd apr-${apr_ver} > /dev/null
-    ./configure --prefix=${apr_install_dir}
-    make -j ${THREAD} && make install
-    popd > /dev/null
-    rm -rf apr-${apr_ver}
-  fi
-
-  tar xzf apache-tomcat-${tomcat10_ver}.tar.gz
-  [ ! -d "${tomcat_install_dir}" ] && mkdir -p ${tomcat_install_dir}
-  /bin/cp -R apache-tomcat-${tomcat10_ver}/* ${tomcat_install_dir}
-  rm -rf ${tomcat_install_dir}/webapps/{docs,examples,host-manager,manager,ROOT/*}
-
-  if [ ! -e "${tomcat_install_dir}/conf/server.xml" ]; then
-    rm -rf ${tomcat_install_dir}
-    echo "${CFAILURE}Tomcat install failed, Please contact the author! ${CEND}" && grep -Ew 'NAME|ID|ID_LIKE|VERSION_ID|PRETTY_NAME' /etc/os-release
+# Check if JAVA_HOME is set
+if [ -z "${JAVA_HOME}" ]; then
+    echo "${CFAILURE}JAVA_HOME environment variable is not set${CEND}"
+    echo "请设置JAVA_HOME环境变量，例如：export JAVA_HOME=/path/to/your/jdk && export PATH=\$JAVA_HOME/bin:\$PATH"
     kill -9 $$; exit 1;
-  fi
+fi
 
-  pushd ${tomcat_install_dir}/bin > /dev/null
-  tar xzf tomcat-native.tar.gz
-  pushd tomcat-native-*-src/native > /dev/null
-  if [ "${armplatform}" == "y" ]; then
-    ./configure --prefix=${apr_install_dir} --with-apr=${apr_install_dir}
-  else
-    ./configure --prefix=${apr_install_dir} --with-apr=${apr_install_dir} --with-ssl=${openssl_install_dir}
-  fi
-  make -j ${THREAD} && make install
-  popd > /dev/null
-  rm -rf tomcat-native-*
-  if [ -e "${apr_install_dir}/lib/libtcnative-1.la" ]; then
-    [ ${Mem} -le 768 ] && let Xms_Mem="${Mem}/3" || Xms_Mem=256
-    let XmxMem="${Mem}/2"
-    cat > ${tomcat_install_dir}/bin/setenv.sh << EOF
+# Check if user is root
+if [ "$(id -u)" != "0" ]; then
+    echo "${CFAILURE}Error: You must be root to run this script${CEND}"
+    exit 1
+fi
+
+# Tomcat version
+tomcat_version="10.1.16"
+# 更新为可用的下载链接
+download_url="https://archive.apache.org/dist/tomcat/tomcat-10/v${tomcat_version}/bin/apache-tomcat-${tomcat_version}.tar.gz"
+# Install directory
+tomcat_install_dir="/usr/local/tomcat"
+# User and group
+tomcat_user="tomcat"
+tomcat_group="tomcat"
+# Memory settings
+Xms_Mem="256"
+XmxMem="512"
+# APR version
+apr_version="1.7.4"
+# APR 下载 URL
+apr_download_url="https://archive.apache.org/dist/apr/apr-${apr_version}.tar.gz"
+# APR install directory
+apr_install_dir="/usr/local/apr"
+
+# Create user and group
+groupadd -r ${tomcat_group}
+useradd -r -g ${tomcat_group} -d ${tomcat_install_dir} -s /sbin/nologin ${tomcat_user}
+
+# 下载 APR
+echo "${CINFO}Installing APR...${CEND}"
+retry=3
+while [ $retry -gt 0 ]; do
+    wget $apr_download_url
+    if [ $? -eq 0 ]; then
+        break
+    fi
+    echo "${CWARNING}APR download failed. Retrying...${CEND}"
+    retry=$((retry - 1))
+    sleep 5
+done
+if [ $retry -eq 0 ]; then
+    echo "${CFAILURE}APR download failed after 3 attempts.${CEND}"
+    exit 1
+fi
+
+tar -zxvf apr-${apr_version}.tar.gz
+cd apr-${apr_version}
+./configure --prefix=${apr_install_dir}
+make && make install
+cd ..
+rm -rf apr-${apr_version} apr-${apr_version}.tar.gz
+
+# 下载 Tomcat
+echo "${CINFO}Downloading and extracting Tomcat...${CEND}"
+retry=3
+while [ $retry -gt 0 ]; do
+    wget $download_url
+    if [ $? -eq 0 ]; then
+        break
+    fi
+    echo "${CWARNING}Tomcat download failed. Retrying...${CEND}"
+    retry=$((retry - 1))
+    sleep 5
+done
+if [ $retry -eq 0 ]; then
+    echo "${CFAILURE}Tomcat download failed after 3 attempts.${CEND}"
+    exit 1
+fi
+
+tar -zxvf apache-tomcat-${tomcat_version}.tar.gz
+mv apache-tomcat-${tomcat_version} ${tomcat_install_dir}
+rm -rf apache-tomcat-${tomcat_version}.tar.gz
+
+# Set permissions
+chown -R ${tomcat_user}:${tomcat_group} ${tomcat_install_dir}
+chmod -R g+r ${tomcat_install_dir}/conf
+chmod g+x ${tomcat_install_dir}/conf
+chown -R ${tomcat_user}:${tomcat_group} ${tomcat_install_dir}/logs ${tomcat_install_dir}/temp ${tomcat_install_dir}/webapps ${tomcat_install_dir}/work
+chown -R ${tomcat_user}:${tomcat_group} ${tomcat_install_dir}/bin ${tomcat_install_dir}/lib
+chmod +x ${tomcat_install_dir}/bin/*.sh
+
+# Set environment variables
+cat > ${tomcat_install_dir}/bin/setenv.sh << EOF
 JAVA_OPTS='-Djava.security.egd=file:/dev/./urandom -server -Xms${Xms_Mem}m -Xmx${XmxMem}m -Dfile.encoding=UTF-8'
 CATALINA_OPTS="-Djava.library.path=${apr_install_dir}/lib"
+# 以下配置项被注释，若需要使用，请取消注释并根据实际情况修改
 # -Djava.rmi.server.hostname=$IPADDR
 # -Dcom.sun.management.jmxremote.password.file=\$CATALINA_BASE/conf/jmxremote.password
 # -Dcom.sun.management.jmxremote.access.file=\$CATALINA_BASE/conf/jmxremote.access
-# -Dcom.sun.management.jmxremote.ssl=false"
+# -Dcom.sun.management.jmxremote.ssl=false
 EOF
-    chmod +x ./*.sh
-    /bin/mv ${tomcat_install_dir}/conf/server.xml{,_bk}
-    popd # goto ${oneinstack_dir}/src
-    /bin/cp ${oneinstack_dir}/config/server.xml ${tomcat_install_dir}/conf
-    sed -i "s@/usr/local/tomcat@${tomcat_install_dir}@g" ${tomcat_install_dir}/conf/server.xml
 
-    if [ ! -e "${nginx_install_dir}/sbin/nginx" -a ! -e "${tengine_install_dir}/sbin/nginx" -a ! -e "${openresty_install_dir}/nginx/sbin/nginx" -a ! -e "${apache_install_dir}/bin/httpd" ]; then
-      if [ "${PM}" == 'yum' ]; then
-        if [ "`firewall-cmd --state`" == "running" ]; then
-          firewall-cmd --permanent --zone=public --add-port=8080/tcp
-          firewall-cmd --reload
-	fi
-      elif [ "${PM}" == 'apt-get' ]; then
-        if ufw status | grep -wq active; then
-            ufw allow 8080/tcp
-        fi
-      fi
-    fi
+# Create systemd service
+cat > /etc/systemd/system/tomcat.service << EOF
+[Unit]
+Description=Apache Tomcat Web Application Container
+After=network.target
 
-    [ ! -d "${tomcat_install_dir}/conf/vhost" ] && mkdir ${tomcat_install_dir}/conf/vhost
-    cat > ${tomcat_install_dir}/conf/vhost/localhost.xml << EOF
-<Host name="localhost" appBase="${wwwroot_dir}/default" unpackWARs="true" autoDeploy="true">
-  <Context path="" docBase="${wwwroot_dir}/default" reloadable="false" crossContext="true"/>
-  <Valve className="org.apache.catalina.valves.AccessLogValve" directory="logs"
-    prefix="localhost_access_log" suffix=".txt" pattern="%h %l %u %t &quot;%r&quot; %s %b" />
-  <Valve className="org.apache.catalina.valves.RemoteIpValve" remoteIpHeader="X-Forwarded-For"
-    protocolHeader="X-Forwarded-Proto" protocolHeaderHttpsValue="https"/>
-</Host>
+[Service]
+Type=forking
+
+Environment=JAVA_HOME=${JAVA_HOME}
+Environment=CATALINA_PID=${tomcat_install_dir}/temp/tomcat.pid
+Environment=CATALINA_HOME=${tomcat_install_dir}
+Environment=CATALINA_BASE=${tomcat_install_dir}
+Environment='CATALINA_OPTS=-Xms${Xms_Mem}m -Xmx${XmxMem}m -server -XX:+UseParallelGC'
+Environment='JAVA_OPTS=-Djava.awt.headless=true -Djava.security.egd=file:/dev/./urandom'
+
+ExecStart=${tomcat_install_dir}/bin/startup.sh
+ExecStop=/bin/kill -15 \$MAINPID
+
+User=${tomcat_user}
+Group=${tomcat_group}
+UMask=0007
+RestartSec=10
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
 EOF
-    # logrotate tomcat catalina.out
-    cat > /etc/logrotate.d/tomcat << EOF
-${tomcat_install_dir}/logs/catalina.out {
-  daily
-  rotate 5
-  missingok
-  dateext
-  compress
-  notifempty
-  copytruncate
-}
-EOF
-    [ -z "$(grep '<user username="admin" password=' ${tomcat_install_dir}/conf/tomcat-users.xml)" ] && sed -i "s@^</tomcat-users>@<role rolename=\"admin-gui\"/>\n<role rolename=\"admin-script\"/>\n<role rolename=\"manager-gui\"/>\n<role rolename=\"manager-script\"/>\n<user username=\"admin\" password=\"$(cat /dev/urandom | head -1 | md5sum | head -c 10)\" roles=\"admin-gui,admin-script,manager-gui,manager-script\"/>\n</tomcat-users>@" ${tomcat_install_dir}/conf/tomcat-users.xml
-    cat > ${tomcat_install_dir}/conf/jmxremote.access << EOF
-monitorRole   readonly
-controlRole   readwrite \
-              create javax.management.monitor.*,javax.management.timer.* \
-              unregister
-EOF
-    cat > ${tomcat_install_dir}/conf/jmxremote.password << EOF
-monitorRole  $(cat /dev/urandom | head -1 | md5sum | head -c 8)
-# controlRole   R&D
-EOF
-    chown -R ${run_user}:${run_group} ${tomcat_install_dir}
-    /bin/cp ${oneinstack_dir}/init.d/Tomcat-init /etc/init.d/tomcat
-    sed -i "s@JAVA_HOME=.*@JAVA_HOME=${JAVA_HOME}@" /etc/init.d/tomcat
-    sed -i "s@^CATALINA_HOME=.*@CATALINA_HOME=${tomcat_install_dir}@" /etc/init.d/tomcat
-    sed -i "s@^TOMCAT_USER=.*@TOMCAT_USER=${run_user}@" /etc/init.d/tomcat
-    [ "${PM}" == 'yum' ] && { chkconfig --add tomcat; chkconfig tomcat on; }
-    [ "${PM}" == 'apt-get' ] && update-rc.d tomcat defaults
-    echo "${CSUCCESS}Tomcat installed successfully! ${CEND}"
-    rm -rf apache-tomcat-${tomcat10_ver}
-  else
-    popd > /dev/null
-    echo "${CFAILURE}Tomcat install failed, Please contact the author! ${CEND}" && grep -Ew 'NAME|ID|ID_LIKE|VERSION_ID|PRETTY_NAME' /etc/os-release
-  fi
-  service tomcat start
-  popd > /dev/null
-}
+
+# Reload systemd manager configuration
+systemctl daemon-reload
+
+# Start and enable Tomcat service
+systemctl start tomcat
+systemctl enable tomcat
+
+echo "${CSUCCESS}Tomcat ${tomcat_version} installed successfully!${CEND}"
